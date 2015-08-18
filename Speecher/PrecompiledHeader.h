53a96326c00b836e8bdab5cda2af9a1ba81b5682
Willy <vilm1s@live.com>
2015-08-18 19:11:33 +0300

    heh

--- Speecher/PrecompiledHeader.h
+++ Speecher/PrecompiledHeader.h
@@ -3,9 +3,7 @@
 #define WINVER 0x0601
 #define _WIN32_WINNT 0x0601
 
-#include <sapi53.h>
 #include <Windows.h>
-#include <wrl.h>
 
 #include <string>
 #include <vector>
--- Speecher/main.cpp
+++ Speecher/main.cpp
@@ -1,22 +1,6 @@
 #include "PrecompiledHeader.h"
 
-struct CoInitializeWrapper
-{
-	CoInitializeWrapper()
-	{
-		auto hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
-		Assert(SUCCEEDED(hr));
-	}
-	
-	~CoInitializeWrapper()
-	{
-		CoUninitialize();
-	}
-};
-
 int CALLBACK wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 {
-	CoInitializeWrapper coInit;
-	
 	
 }
